96e740827f04938bb9f9caa7e501bf7275ccfd28
AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>
2021-03-18 11:40:29 +0300

    add template for lesson_2

---
+++ algorithms/src/test/java/lesson_2/LinkedList2Test.java
@@ -0,0 +1,36 @@
+package lesson_2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedList2Test {
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void count() {
+    }
+
+    @Test
+    void insertAfter() {
+    }
+}
