ba66f2d3408892789853e3404067e7921db84a40
AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>
2021-03-24 20:23:40 +0300

    add template DynArray and DynArrayTest

---
+++ algorithms/src/test/java/lesson_3/DynArrayTest.java
@@ -0,0 +1,28 @@
+package lesson_3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DynArrayTest {
+
+    @Test
+    void makeArray() {
+    }
+
+    @Test
+    void getItem() {
+    }
+
+    @Test
+    void append() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void remove() {
+    }
+}
