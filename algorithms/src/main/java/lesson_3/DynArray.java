dd9f14f57d16c269efab202b9d0f1c4521e5787a
ALDEGA <anyukhin.artem@gmail.com>
2021-03-25 10:51:29 +0300

    Section 1 lesson 3
    * add template DynArray and DynArrayTest
    * add makeArray() method
    * add other methods
    * add complicity
    Co-authored-by: AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>

---
+++ algorithms/src/main/java/lesson_3/DynArray.java
@@ -0,0 +1,89 @@
+package lesson_3;
+
+public class DynArray<T>
+{
+    public T [] array;
+    public int count;
+    public int capacity;
+    Class clazz;
+
+    private final double FILL_PERCENTAGE = 0.5d;
+
+    public DynArray(Class clz)
+    {
+        clazz = clz; // нужен для безопасного приведения типов
+        // new DynArray<Integer>(Integer.class);
+
+        count = 0;
+        makeArray(16);
+    }
+
+    public void makeArray(int new_capacity)
+    {
+        T [] newArray = (T[]) java.lang.reflect.Array.newInstance(this.clazz, new_capacity);
+        for (int i = 0; i < count; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+        capacity = new_capacity;
+    }
+
+    private void expand() {
+        int newCapacity = capacity * 2;
+        makeArray(newCapacity);
+    }
+
+    private void compress() {
+        int newCapacity = Math.max((int) (capacity / 1.5), 16);
+        makeArray(newCapacity);
+    }
+
+    //Сложность O(1)
+    public T getItem(int index)
+    {
+        if (index < 0 || index >= count) throw new IndexOutOfBoundsException();
+        return array[index];
+    }
+
+    //Сложность в среднем O(1)
+    public void append(T itm)
+    {
+        if (count == capacity) {
+            expand();
+        }
+        array[count] = itm;
+        count = count + 1;
+    }
+
+    //Сложность в среднем O(N)
+    public void insert(T itm, int index)
+    {
+        if (index < 0 || index > count) throw new IndexOutOfBoundsException();
+
+        if (count == capacity) {
+            expand();
+        }
+        for (int i = index; i <= count; i++) {
+            T temp = array[i];
+            array[i] = itm;
+            itm = temp;
+        }
+        count = count + 1;
+    }
+
+    //Сложность в среднем O(N)
+    public void remove(int index)
+    {
+        if (index < 0 || index >= count) throw new IndexOutOfBoundsException();
+
+        for (int i = index; i < count - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        count = count - 1;
+
+        if (count * 1.0d / capacity < FILL_PERCENTAGE) {
+            compress();
+        }
+    }
+
+}
