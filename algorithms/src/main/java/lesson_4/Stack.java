027b8cd198a53ba59b62886b6220d9ac7ef91129
AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>
2021-03-28 19:55:26 +0300

    refactoring

--- algorithms/src/main/java/lesson_4/Stack.java
+++ algorithms/src/main/java/lesson_4/Stack.java
@@ -2,33 +2,27 @@ package lesson_4;
 
 import java.util.*;
 
-public class Stack<T>
-{
+public class Stack<T> {
 
     public LinkedList<T> stack;
 
-    public Stack()
-    {
+    public Stack() {
         this.stack = new LinkedList<>();
     }
 
-    public int size()
-    {
-        return stack.size();
+    public int size() {
+        return this.stack.size(); //сложность O(1)
     }
 
-    public T pop()
-    {
-        return stack.poll();
+    public T pop() {
+        return this.stack.poll(); //сложность O(1)
     }
 
-    public void push(T val)
-    {
-        stack.push(val);
+    public void push(T val) {
+        this.stack.push(val); //сложность O(1)
     }
 
-    public T peek()
-    {
-        return stack.peek();
+    public T peek() {
+        return this.stack.peek(); //сложность O(1)
     }
 }
