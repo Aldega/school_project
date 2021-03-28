1d407332b3648e17954bd3f297ac44aaf372b3be
AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>
2021-03-28 19:44:24 +0300

    add method which calculate postfix expression

--- algorithms/src/main/java/lesson_4/StackUtils.java
+++ algorithms/src/main/java/lesson_4/StackUtils.java
@@ -20,4 +20,33 @@ public class StackUtils {
         }
         return stack.peek() == null;
     }
+
+    public static Integer calculate(Stack<String> expression) {
+        Stack<Integer> result = new Stack<>();
+        while (expression.peek() != null) {
+            String value = expression.pop();
+            if (value.matches("\\d+")) { //if value is int number
+                 result.push(Integer.parseInt(value));
+            }
+            if (value.matches("[*]")) { //if value is multiply
+                Integer calcRes = result.pop();
+                while(result.size() > 0) {
+                    calcRes *= result.pop();
+                }
+                result.push(calcRes);
+            }
+            if (value.matches("[+]")) { //if value is sum
+                Integer calcRes = result.pop();
+                while(result.size() > 0) {
+                    calcRes += result.pop();
+                }
+                result.push(calcRes);
+            }
+            if (value.matches("[=]")) { //if value is equal
+                return result.pop();
+            }
+        }
+        return result.pop();
+    }
+
 }
--- algorithms/src/test/java/lesson_4/StackUtilsTest.java
+++ algorithms/src/test/java/lesson_4/StackUtilsTest.java
@@ -2,6 +2,7 @@ package lesson_4;
 
 import org.junit.jupiter.api.Test;
 
+import static lesson_4.StackUtils.calculate;
 import static lesson_4.StackUtils.isBalanced;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,5 +28,34 @@ class StackUtilsTest {
         assertFalse(isBalanced("))(("));
         assertFalse(isBalanced("((())"));
         assertFalse(isBalanced(")(())(()))(()"));
+    }
+
+    @Test
+    void calculateTest() {
+        Stack<String> expression = new Stack<>();
+        expression.push("=");
+        expression.push("+");
+        expression.push("9");
+        expression.push("*");
+        expression.push("5");
+        expression.push("+");
+        expression.push("2");
+        expression.push("8");
+        assertEquals(59, calculate(expression));
+
+        expression = new Stack<>();
+        expression.push("=");
+        expression.push("*");
+        expression.push("2");
+        expression.push("+");
+        expression.push("1");
+        expression.push("2");
+        expression.push("+");
+        expression.push("3");
+        expression.push("4");
+        expression.push("+");
+        expression.push("5");
+        assertEquals(30, calculate(expression));
+
     }
 }
