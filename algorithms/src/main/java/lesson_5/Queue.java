69d701f9171ec82b11e9758771d91a96db62615a
AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>
2021-03-30 08:24:56 +0300

    format Queue

--- algorithms/src/main/java/lesson_5/Queue.java
+++ algorithms/src/main/java/lesson_5/Queue.java
@@ -2,27 +2,22 @@ package lesson_5;
 
 import java.util.*;
 
-public class Queue<T>
-{
+public class Queue<T> {
     public LinkedList<T> queue;
 
-    public Queue()
-    {
+    public Queue() {
         this.queue = new LinkedList<>();
     }
 
-    public void enqueue(T item)
-    {
+    public void enqueue(T item) {
         queue.addLast(item);
     }
 
-    public T dequeue()
-    {
+    public T dequeue() {
         return queue.pollFirst();
     }
 
-    public int size()
-    {
+    public int size() {
         return queue.size();
     }
 
