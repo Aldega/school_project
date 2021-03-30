e27261d3cd80ed8f584524ea297684c18730e364
AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>
2021-03-30 08:02:52 +0300

    add template and test class for Queue

---
+++ algorithms/src/test/java/lesson_5/QueueTest.java
@@ -0,0 +1,20 @@
+package lesson_5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueTest {
+
+    @Test
+    void enqueue() {
+    }
+
+    @Test
+    void dequeue() {
+    }
+
+    @Test
+    void size() {
+    }
+}
