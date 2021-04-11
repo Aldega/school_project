c320f1858c2c4a8c9e080a6f71ef925492f328c6
AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>
2021-04-11 08:16:52 +0300

    Add template and tests for HashTable

---
+++ algorithms/src/test/java/lesson_8/HashTableTest.java
@@ -0,0 +1,24 @@
+package lesson_8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashTableTest {
+
+    @Test
+    void hashFun() {
+    }
+
+    @Test
+    void seekSlot() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void find() {
+    }
+}
