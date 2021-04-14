cc90168fafa4a97f90552796c4700da177912dcc
ALDEGA <anyukhin.artem@gmail.com>
2021-04-14 06:46:40 +0300

    Section 1 lesson 8 (#8)
    * Add template and tests for HashTable
    * hashFun method and tests
    * seekSlot method and tests
    * find and put methods and tests
    
    Co-authored-by: AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>

---
+++ algorithms/src/main/java/lesson_8/HashTable.java
@@ -0,0 +1,69 @@
+package lesson_8;
+
+public class HashTable {
+    public int size;
+    public int step;
+    public String[] slots;
+
+    public HashTable(int sz, int stp) {
+        size = sz;
+        step = stp;
+        slots = new String[size];
+        for (int i = 0; i < size; i++) slots[i] = null;
+    }
+
+    public int hashFun(String value) {
+        return value.hashCode() % size;
+    }
+
+    public int seekSlot(String value) {
+        int firstSlot = hashFun(value);
+        int index = firstSlot;
+
+        while (isNotOverhead(index, firstSlot)) {
+            int slot = getSlot(index);
+            if (slots[slot] == null) {
+                return slot;
+            }
+            index = index + step;
+        }
+        return -1;
+    }
+
+    public int put(String value) {
+        int slot = seekSlot(value);
+
+        if (slot != -1) {
+            slots[slot] = value;
+        }
+
+        return slot;
+    }
+
+    public int find(String value) {
+
+        int firstSlot = hashFun(value);
+        int index = firstSlot;
+
+        while (isNotOverhead(index, firstSlot)) {
+            int slot = getSlot(index);
+            if (value.equals(slots[slot])) {
+                return slot;
+            }
+            index = index + step;
+        }
+        return -1;
+    }
+
+    private int getCicleCount(int index) {
+        return index / size;
+    }
+
+    private int getSlot(int index) {
+        return index % size;
+    }
+
+    private boolean isNotOverhead(int index, int firstSlot) {
+        return getCicleCount(index) < 1 || getSlot(index) < firstSlot;
+    }
+}
