55f030c2facc0b94edb8092167d1c13c4e11d88e
AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>
2021-04-14 21:40:09 +0300

    add template for NativeDictionary class and tests

---
+++ algorithms/src/test/java/lesson_9/NativeDictionaryTest.java
@@ -0,0 +1,24 @@
+package lesson_9;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NativeDictionaryTest {
+
+    @Test
+    void hashFun() {
+    }
+
+    @Test
+    void isKey() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void get() {
+    }
+}
