b5cc7c8bc8a34929e9c331acaf9deb6f7aef5dd3
AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>
2021-04-23 23:19:00 +0300

    Add template and tests

---
+++ algorithms/src/test/java/lesson_10/PowerSetTest.java
@@ -0,0 +1,40 @@
+package lesson_10;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PowerSetTest {
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void intersection() {
+    }
+
+    @Test
+    void union() {
+    }
+
+    @Test
+    void difference() {
+    }
+
+    @Test
+    void isSubset() {
+    }
+}
