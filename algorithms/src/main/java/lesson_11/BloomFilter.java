803f561b125a46d52be00a15e896fff3bb4d010a
AnyukhinArtem <Anyukhin.A.Yur@sberbank.ru>
2021-04-28 01:20:08 +0300

    implements methods and test

--- algorithms/src/main/java/lesson_11/BloomFilter.java
+++ algorithms/src/main/java/lesson_11/BloomFilter.java
@@ -1,35 +1,38 @@
 package lesson_11;
 
 public class BloomFilter {
-    public int filter_len;
 
+    public int filter_len;
+    public int bitArray = 0;
+
+    // создаём битовый массив длиной f_len ...
     public BloomFilter(int f_len) {
         filter_len = f_len;
-        // создаём битовый массив длиной f_len ...
     }
 
     // хэш-функции
     public int hash1(String str1) {
-        // 17
-        for (int i = 0; i < str1.length(); i++) {
-            int code = (int) str1.charAt(i);
-        }
-        // реализация ...
-        return 0;
+        return hash(str1, 17);
     }
 
     public int hash2(String str1) {
-        // 223
-        // реализация ...
-        return 0;
+        return hash(str1, 223);
     }
 
     public void add(String str1) {
-        // добавляем строку str1 в фильтр
+        bitArray = bitArray | (1 << hash1(str1)) | (1 << hash2(str1));
     }
 
     public boolean isValue(String str1) {
         // проверка, имеется ли строка str1 в фильтре
-        return false;
+        return (bitArray & (1 << hash1(str1))) != 0 && (bitArray & (1 << hash2(str1))) != 0;
+    }
+
+    private int hash(String str1, int randomValue) {
+        int code = 0;
+        for (int i = 0; i < str1.length(); i++) {
+            code = Math.abs(code * randomValue + (int) str1.charAt(i));
+        }
+        return code % filter_len;
     }
 }
--- algorithms/src/test/java/lesson_11/BloomFilterTest.java
+++ algorithms/src/test/java/lesson_11/BloomFilterTest.java
@@ -2,12 +2,40 @@ package lesson_11;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BloomFilterTest {
 
     @Test
     void hash1() {
+        BloomFilter bloomFilter = new BloomFilter(32);
+        List<String> strings = Stream.of("0123456789"
+                , "1234567890"
+                , "2345678901"
+                , "3456789012"
+                , "4567890123"
+                , "5678901234"
+                , "6789012345"
+                , "7890123456"
+                , "8901234567"
+                , "9012345678").collect(toList());
+
+        for (int i = 0; i < strings.size(); i++) {
+
+            System.out.println(i);
+            System.out.println(bloomFilter.hash1(strings.get(i)));
+            System.out.println(bloomFilter.hash2(strings.get(i)));
+            System.out.println(bloomFilter.isValue(strings.get(i)));
+            bloomFilter.add(strings.get(i));
+            System.out.println(bloomFilter.isValue(strings.get(i)));
+            System.out.println(Integer.toBinaryString(bloomFilter.bitArray) + "\n");
+        }
+
     }
 
     @Test
